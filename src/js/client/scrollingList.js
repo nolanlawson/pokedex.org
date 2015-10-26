d9398e61665245649d691ddf645fc0fec92ce211
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-26 09:14:12 -0400

    remove fade in/fade out entirely

--- src/js/client/scrollingList.js
+++ src/js/client/scrollingList.js
@@ -22,95 +22,21 @@ function applyPatch(patchString) {
   progress.end();
 }
 
-function onMessage(message) {
+function onFiltered(message) {
   console.timeEnd('worker-filter');
   console.log('worker sent message');
 
   if (message.type === 'monstersListPatch') {
     applyPatch(message.content);
-    renderSprites();
   }
 }
 
-function binarySearchForFirstVisibleChild(children) {
-  var low = 0, high = children.length, mid, rect, val;
-  while (low < high) {
-    mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
-    rect = children[mid].getBoundingClientRect();
-    val = rect.top + rect.height;
-    val < 0 ? low = mid + 1 : high = mid
+function onMessage(message) {
+  if (message.type === 'monstersListPatch') {
+    onFiltered(message);
   }
-  return low;
 }
-
-function renderSprites() {
-  monstersList = monstersList || $('#monsters-list');
-  var children = monstersList.children;
-  if (!children.length) {
-    return;
-  }
-  console.time('renderSprites');
-  console.time('binarySearch');
-  var windowHeight = window.innerHeight;
-  var numShown = 0;
-  var numHidden = 0;
-  var firstVisibleIndex = binarySearchForFirstVisibleChild(children);
-  console.timeEnd('binarySearch');
-  console.log('firstVisibleIndex', firstVisibleIndex);
-  var i = -1;
-  var done = false;
-  console.time('loop');
-  while (++i < children.length) {
-    var child = children[i];
-    if (i < firstVisibleIndex) {
-      // before the visible viewport
-      //child.classList.add('hidden');
-      numHidden++;
-      continue;
-    }
-    if (done) {
-      // after the visible viewport
-      //child.classList.add('hidden');
-      numHidden++;
-      continue;
-    }
-    console.time('getBoundingClientRect');
-    // possibly within the visible viewport
-    var rect = child.getBoundingClientRect();
-    console.timeEnd('getBoundingClientRect');
-    if ((rect.bottom - rect.height) < windowHeight) {
-      //child.classList.remove('hidden');
-      numShown++;
-      continue;
-    }
-    //child.classList.add('hidden');
-    numHidden++;
-    done = true;
-  }
-  console.timeEnd('loop');
-  console.log('numShown', numShown, 'numHidden', numHidden);
-  console.timeEnd('renderSprites');
-}
-
-renderSprites();
 
 worker.addEventListener('message', e => {
   onMessage(e.data);
 });
-
-// use a higher debounce for safari, which seems to ignore it if you set it
-// too low
-var isSafari = typeof openDatabase !== 'undefined' &&
-  /(Safari|iPhone|iPad|iPod)/.test(navigator.userAgent) &&
-  !/Chrome/.test(navigator.userAgent) &&
-  !/BlackBerry/.test(navigator.platform);
-var delay = isSafari ? 100 : 20;
-document.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('scroll', debounce(renderSprites, delay));
-});
-
-// This happens e.g. when the keyboard moves in/out on Android, in which
-// case the window.innerHeight also changes, so we need to recalculate.
-// This is debounced for the benefit of web developers manually resizing
-// their window, so it doesn't end up looking so janky.
-window.addEventListener('resize', debounce(renderSprites, 50));
