5ec37ab7b50c55d7fb61145239f39d735ce435e9
Nolan Lawson <nolan.lawson@gmail.com>
2015-11-02 21:08:16 -0500

    add db files

---
+++ src/js/shared/db/keyValueStore.js
@@ -0,0 +1,11 @@
+// simple key-value store using localforage
+
+var localForage = require('localforage');
+
+var keyValueStore = {
+  get: key => localForage.getItem(key),
+  set: (key, val) => localForage.setItem(key, val),
+  delete: key => localForage.removeItem(key, val)
+};
+
+module.exports = keyValueStore;
