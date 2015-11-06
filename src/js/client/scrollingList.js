76819ebbd9dc1563c682f82b419b24c70ebd4076
Nolan Lawson <nolan.lawson@gmail.com>
2015-11-05 22:23:28 -0500

    add scroll hiding behavior back in

--- src/js/client/scrollingList.js
+++ src/js/client/scrollingList.js
@@ -1,12 +1,83 @@
 var progress = require('./progress');
 var fromJson = require('vdom-as-json/fromJson');
 var patchElement = require('virtual-dom/patch');
+var debounce = require('debounce');
 var $ = document.querySelector.bind(document);
 
 var detailViewOrchestrator = require('./detailViewOrchestrator');
 var worker = require('./worker');
 
 var monstersList;
+
+// use a higher debounce for safari, which seems to ignore it if you set it
+// too low
+var isSafari = typeof openDatabase !== 'undefined' &&
+  /(Safari|iPhone|iPad|iPod)/.test(navigator.userAgent) &&
+  !/Chrome/.test(navigator.userAgent) &&
+  !/BlackBerry/.test(navigator.platform);
+var delay = isSafari ? 100 : 20;
+
+function binarySearchForFirstVisibleChild(children) {
+  var low = 0, high = children.length, mid, rect, val;
+  while (low < high) {
+    mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
+    rect = children[mid].getBoundingClientRect();
+    val = rect.top + rect.height;
+    val < 0 ? low = mid + 1 : high = mid
+  }
+  return low;
+}
+
+function renderSprites() {
+  monstersList = monstersList || $('#monsters-list');
+  var children = monstersList.children;
+  if (!children.length) {
+    return;
+  }
+  console.time('renderSprites');
+  console.time('binarySearch');
+  var windowHeight = window.innerHeight;
+  var numShown = 0;
+  var numHidden = 0;
+  var firstVisibleIndex = binarySearchForFirstVisibleChild(children);
+  console.timeEnd('binarySearch');
+  console.log('firstVisibleIndex', firstVisibleIndex);
+  var i = -1;
+  var done = false;
+  console.time('loop');
+  while (++i < children.length) {
+    var child = children[i];
+    if (i < firstVisibleIndex) {
+      // before the visible viewport
+      child.classList.add('hidden');
+      numHidden++;
+      continue;
+    }
+    if (done) {
+      // after the visible viewport
+      child.classList.add('hidden');
+      numHidden++;
+      continue;
+    }
+    console.time('getBoundingClientRect');
+    // possibly within the visible viewport
+    var rect = child.getBoundingClientRect();
+    console.timeEnd('getBoundingClientRect');
+    if ((rect.bottom - rect.height) < windowHeight) {
+      child.classList.remove('hidden');
+      numShown++;
+      continue;
+    }
+    child.classList.add('hidden');
+    numHidden++;
+    done = true;
+  }
+  console.timeEnd('loop');
+  console.log('numShown', numShown, 'numHidden', numHidden);
+  console.timeEnd('renderSprites');
+}
+
+renderSprites();
 
 function applyPatch(patchString) {
   console.time('JSON.parse()');
@@ -53,4 +124,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     detailViewOrchestrator.animateInPartOne(nationalId);
   });
+  window.addEventListener('scroll', debounce(renderSprites, delay));
 }, false);
+
+// This happens e.g. when the keyboard moves in/out on Android, in which
+// case the window.innerHeight also changes, so we need to recalculate.
+// This is debounced for the benefit of web developers manually resizing
+// their window, so it doesn't end up looking so janky.
+window.addEventListener('resize', debounce(renderSprites, 50));
