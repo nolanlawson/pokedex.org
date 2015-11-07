97d56c9eb9975b0f5579be1b75113c83cd5fe977
Nolan Lawson <nolan.lawson@gmail.com>
2015-11-07 16:15:43 -0500

    fix service worker

--- src/js/serviceWorker/index.js
+++ src/js/serviceWorker/index.js
@@ -13,13 +13,14 @@ var version = '1.0.0';
 
 var staticContent = [
   '/',
+  '/index.html',
   '/js/worker.js',
   '/js/main.js',
   '/manifest.json',
   '/favicon.ico',
   '/img/icon-48.png',
   '/img/icon-96.png',
-  '/img/icon-192.png',
+  '/img/icon-192.png'
 ];
 
 if (process.env.NODE_ENV === 'development') {
