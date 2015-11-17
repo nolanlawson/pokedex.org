8d3fad4ffb5fd9f8f3a61080a126cdd10d2fdab3
Nolan Lawson <nolan.lawson@gmail.com>
2015-11-16 22:20:08 -0500

    try show/hiding list elements

--- src/js/client/main/scrollingList.js
+++ src/js/client/main/scrollingList.js
@@ -5,10 +5,12 @@ var detailViewOrchestrator = require('./detailViewOrchestrator');
 var worker = require('./../shared/worker');
 var rippleEffect = require('./rippleEffect');
 var debounce = require('debounce');
-var DEBOUNCE_DELAY = 50;
-var SCROLL_PREFETCH_OFFSET = 400;
+var DEBOUNCE_DELAY = 100;
+var SCROLL_PREFETCH_OFFSET = 800;
+var PLACEHOLDER_OFFSET = 20;
 var $ = document.querySelector.bind(document);
 
+var placeholderListItems = new Array(30);
 var monstersList;
 var footerHeight;
 var endOfList;
@@ -31,6 +33,7 @@ function onMonstersListPatch(message) {
 
   doApplyPatch(message.patch);
   endOfList = message.endOfList;
+  placeholderListItems = new Array(monstersList.children.length);
   onScroll(); // sometimes need to check the scroll twice, for large screens
 }
 
@@ -50,7 +53,86 @@ function scrolledToBottom() {
     (document.body.scrollHeight - footerHeight - SCROLL_PREFETCH_OFFSET);
 }
 
+function binarySearchForFirstVisibleChild(children) {
+  var low = 0, high = children.length, mid, rect, val;
+  while (low < high) {
+    mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
+    rect = children[mid].getBoundingClientRect();
+    val = rect.bottom;
+    val < 0 ? low = mid + 1 : high = mid
+  }
+  return low;
+}
+
+function binarySearchForFirstInvisibleChild(start, children) {
+  var windowHeight = window.innerHeight;
+  var low = start, high = children.length, mid, rect, val;
+  while (low < high) {
+    mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
+    rect = children[mid].getBoundingClientRect();
+    val = rect.top;
+    val < windowHeight ? low = mid + 1 : high = mid
+  }
+  return low;
+}
+
+function togglePlaceholder(childElement, i, placeholder) {
+  if (!!placeholderListItems[i] === placeholder) {
+    return; // don't need to modify class list
+  } else if (placeholder) {
+    childElement.classList.add('placeholder');
+    placeholderListItems[i] = true;
+  } else {
+    childElement.classList.remove('placeholder');
+    placeholderListItems[i] = false;
+  }
+}
+
+function renderSprites() {
+  var children = monstersList.children;
+  if (!children.length) {
+    return;
+  }
+  console.time('renderSprites');
+  console.time('binarySearch');
+  var numShown = 0;
+  var numHidden = 0;
+  var firstVisibleIndex = binarySearchForFirstVisibleChild(children);
+  var firstInvisibleIndex = binarySearchForFirstInvisibleChild(firstVisibleIndex, children);
+  console.timeEnd('binarySearch');
+  console.log('firstVisibleIndex', firstVisibleIndex);
+  console.log('firstInvisibleIndex', firstInvisibleIndex);
+  var i = -1;
+  console.time('loop');
+
+  while (++i < children.length) {
+    var child = children[i];
+    if (i < firstVisibleIndex) {
+      // before the visible viewport
+      togglePlaceholder(child, i, true);
+      numHidden++;
+    } else if (i >= firstInvisibleIndex) {
+      // after the visible viewport
+      togglePlaceholder(child, i, true);
+      numHidden++;
+    } else {
+      // within the visible viewport
+      togglePlaceholder(child, i, false);
+      numShown++;
+    }
+  }
+  console.timeEnd('loop');
+  console.log('numShown', numShown, 'numHidden', numHidden);
+  console.timeEnd('renderSprites');
+}
+
 function onScroll() {
+  if (!monstersList) {
+    return;
+  }
+
+  renderSprites();
+
   if (!appending && scrolledToBottom()) {
     console.log('scrolledToBottom');
     if (endOfList) {
