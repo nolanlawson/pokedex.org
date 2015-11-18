6a4d41f81f83504de2288382937b3b281a7c585d
Nolan Lawson <nolan.lawson@gmail.com>
2015-11-17 20:09:43 -0500

    increment sw version

--- src/js/serviceWorker/index.js
+++ src/js/serviceWorker/index.js
@@ -12,7 +12,7 @@ var range = require('lodash/utility/range');
 // a: version-isolated change, don't let both versions run together
 // b: new feature
 // c: bug fix
-var version = '1.0.3';
+var version = '1.0.4';
 
 var staticContent = [
   '/',
