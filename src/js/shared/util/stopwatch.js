a8235d9d8783b4354198783d9de36342e245bb3d
Nolan Lawson <nolan.lawson@gmail.com>
2015-11-26 11:42:33 -0800

    log total time correctly

--- src/js/shared/util/stopwatch.js
+++ src/js/shared/util/stopwatch.js
@@ -1,7 +1,9 @@
 function Stopwatch(str) {
   this.startTime = Date.now();
+  this.name = str;
   this.lastStart = str;
   console.time(str);
+  console.time(str + ' - total');
 }
 
 Stopwatch.prototype.time = function (str) {
@@ -12,7 +14,7 @@ Stopwatch.prototype.time = function (str) {
 
 Stopwatch.prototype.totalTime = function (str) {
   console.timeEnd(this.lastStart);
-  console.log(str, 'took ' + (Date.now() - this.startTime) + 'ms');
+  console.timeEnd(this.name + ' - total');
 };
 
 module.exports = Stopwatch;
